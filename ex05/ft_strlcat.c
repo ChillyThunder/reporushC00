0efd73ef0a58d542b2db84aa2740c7208449d740
Sofiapiacenti <115868961+Sofiapiacenti@users.noreply.github.com>
2022-11-03 16:27:24 +0100

    Add files via upload

---
+++ ex05/ft_strlcat.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: spiacent <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/31 16:08:48 by spiacent          #+#    #+#             */
+/*   Updated: 2022/11/02 10:29:13 by spiacent         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+	unsigned int	j;
+	unsigned int	dlen;
+	unsigned int	slen;
+
+	i = 0;
+	j = 0;
+	while (dest[j] != '\0')
+	{
+		j++;
+	}
+	dlen = j;
+	slen = ft_strlen(src);
+	if (size == 0 || size <= dlen)
+		return (slen + size);
+	while (src [i] != '\0' && i < size - dlen - 1)
+	{
+		dest[j] = src[i];
+		i++;
+		j++;
+	}
+	dest[j] = '\0';
+	return (dlen + slen);
+}
+
+/*int main	(void)
+{
+	char src[] = "Ciao sono Sofia";
+    	char dest [] = "45443442";
+    printf("%i \n", ft_strlcat(dest, src, 20));
+    printf("%s \n", dest);
+}*/
